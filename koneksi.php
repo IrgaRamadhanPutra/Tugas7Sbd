6e66f1fb7f468db8ca3f374e63a07bd76f5f1af4
IrgaRamadhanPutra <101645216+IrgaRamadhanPutra@users.noreply.github.com>
2022-06-11 16:58:18 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,12 @@
 <?php
 $host = "localhost";
 $user = "admin_klinik";
-$pass = "312010071";
+$pass = "312010067";
 $db = "klinik_312010067";
-$conn = mysqli_connect('localhost','admin_klinik','312010067','klinik_312010067');
-
+$conn = mysqli_connect($host, $user, $pass, $db);
 if ($conn == false)
 {
-
-	echo "Koneksi ke server gagal.";
-	die();
-
-} else echo "Koneksi berhasil";
+echo "Koneksi ke server gagal.";
+die();
+} #else echo "";
 ?>
